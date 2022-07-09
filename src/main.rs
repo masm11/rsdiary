ca58627fe314b6d5b771a86ab62137e6c004b0a5
Yuuki Harano <masm+github@masm11.me>
2022-07-09 23:54:15 +0900

    ファイルに出力

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::path::PathBuf;
 use std::collections::HashSet;
+use std::fs::File;
+use std::io::prelude::*;   // write_all
+use std::path::Path;
 use sudachi::prelude::MorphemeList;
 use sudachi::config::Config;
 use sudachi::analysis::Mode;
@@ -42,9 +45,21 @@ fn tokenize(string: String, dict: &JapaneseDictionary) -> HashSet<String> {
 fn main() {
     let dict = get_dict();
 
+    let index_path = Path::new("index.txt");
+    let mut file = match File::create(&index_path) {
+	Err(why) => panic!("couldn't create {}: {}", index_path.display(), why),
+	Ok(file) => file,
+    };
+
     let ss = String::from("今日は東京駅に行きます。よろしくお願いします。");
+
     let set = tokenize(ss, &dict);
     for s in set.iter() {
-	println!("{}", s);
+	if let Err(why) = file.write_all(s.as_bytes()) {
+            panic!("couldn't write to {}: {}", index_path.display(), why);
+	}
+	if let Err(why) = file.write_all(b" ") {
+            panic!("couldn't write to {}: {}", index_path.display(), why);
+	}
     }
 }
