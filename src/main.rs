b1438941b493fbb85ce2d30fd1dd4bb198cfe2e7
Yuuki Harano <masm+github@masm11.me>
2022-07-09 23:17:40 +0900

    関数化

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use std::path::PathBuf;
+use std::collections::HashSet;
 use sudachi::prelude::MorphemeList;
 use sudachi::config::Config;
 use sudachi::analysis::Mode;
 use sudachi::analysis::stateful_tokenizer::StatefulTokenizer;
 use sudachi::dic::dictionary::JapaneseDictionary;
 
-fn main() {
-    let ss = "今日は東京駅に行きます。";
-
+fn tokenize(string: String) -> HashSet<String> {
     let config = Config::new(
 	Some(PathBuf::from("./t/sudachi.rs/resources/sudachi.json")),
 	Some(PathBuf::from("./t/sudachi.rs/resources")),
@@ -17,13 +16,24 @@ fn main() {
 	.unwrap_or_else(|e| panic!("Failed to create dictionary: {:?}", e));
 
     let mut analyzer = StatefulTokenizer::new(&dict, Mode::C);
-    analyzer.reset().push_str(ss);
+    analyzer.reset().push_str(&string[..]);
     analyzer.do_tokenize()
 	.unwrap_or_else(|_| panic!("Failed to tokenize."));
     let mut morphs = MorphemeList::empty(analyzer.dict_clone());
     morphs.collect_results(&mut analyzer)
 	.unwrap_or_else(|_| panic!("Failed to collect results."));
+    let mut set = HashSet::<String>::new();
     for m in morphs.iter() {
-	println!("{:?} {:?}", m.surface(), m.normalized_form());
+	set.insert(m.surface().to_string());
+	set.insert(m.normalized_form().to_string());
+    }
+    set
+}
+
+fn main() {
+    let ss = String::from("今日は東京駅に行きます。");
+    let set = tokenize(ss);
+    for s in set.iter() {
+	println!("{}", s);
     }
 }
