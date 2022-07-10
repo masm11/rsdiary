e755652986014430219ed9e3afa919a28c75758a
Yuuki Harano <masm+github@masm11.me>
2022-07-10 15:48:40 +0900

    引数を使うようにした

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::path::PathBuf;
 use std::collections::HashSet;
 use std::fs::File;
@@ -8,6 +9,12 @@ use sudachi::config::Config;
 use sudachi::analysis::Mode;
 use sudachi::analysis::stateful_tokenizer::StatefulTokenizer;
 use sudachi::dic::dictionary::JapaneseDictionary;
+
+fn replace_lf(buf: &String) -> String {
+    buf.replace("\n", " ")
+	.replace("\r", " ")
+	.replace("\t", " ")
+}
 
 fn get_dict() -> JapaneseDictionary {
     let config = Config::new(
@@ -46,20 +53,41 @@ fn main() {
     let dict = get_dict();
 
     let index_path = Path::new("index.txt");
-    let mut file = match File::create(&index_path) {
+    let mut index = match File::create(&index_path) {
 	Err(why) => panic!("couldn't create {}: {}", index_path.display(), why),
-	Ok(file) => file,
+	Ok(index) => index,
     };
 
-    let ss = String::from("今日は東京駅に行きます。よろしくお願いします。");
-
-    let set = tokenize(ss, &dict);
-    for s in set.iter() {
-	if let Err(why) = file.write_all(s.as_bytes()) {
-            panic!("couldn't write to {}: {}", index_path.display(), why);
+    for inp in &env::args().collect::<Vec<String>>()[1..] {
+	let inp_path = Path::new(&inp);
+	let mut file = match File::open(&inp_path) {
+	    Err(why) => panic!("couldn't open {}: {}", inp, why),
+	    Ok(file) => file,
+	};
+	let mut buf = String::new();
+	if let Err(why) = file.read_to_string(&mut buf) {
+	    panic!("couldn't read {}: {}", inp_path.display(), why);
 	}
-	if let Err(why) = file.write_all(b" ") {
-            panic!("couldn't write to {}: {}", index_path.display(), why);
+	
+	let buf = replace_lf(&buf);
+
+	if let Err(why) = index.write_all(inp.as_bytes()) {
+	    panic!("couldn't write to {}: {}", index_path.display(), why);
+	}
+	if let Err(why) = index.write_all("\t".as_bytes()) {
+	    panic!("couldn't write to {}: {}", index_path.display(), why);
+	}
+	
+	let set = tokenize(buf, &dict);
+	
+	for s in set.iter() {
+	    if let Err(why) = index.write_all(s.as_bytes()) {
+		panic!("couldn't write to {}: {}", index_path.display(), why);
+	    }
+	    if let Err(why) = index.write_all(b" ") {
+		panic!("couldn't write to {}: {}", index_path.display(), why);
+	    }
 	}
     }
+
 }
